a4aefcad5e494de7f51d009e6b0d03d68bf37959
kishore <kishorejaipal477@gmail.com>
2022-08-13 22:28:51 -0700

    send email and reset password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,95 +3,144 @@ import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt'
 import { body, validationResult } from 'express-validator';
 
-class UserController{
+class UserController {
     // User Register
-    static UserRegisteration = async(req,res)=>{
-          // handle request body  parameters error
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-        const {name,email,password,password_confirm, tc} = req.body
-        const emailfind =  await  User.findOne({email}) 
-        if(emailfind){
-            res.send("Email already exists, please enter another email")
-        }else{
-        if(name &&  email && password && password_confirm && tc ){
-
-if( password === password_confirm){
-    const salt =await  bcrypt.genSalt(10)
-    const hashPass = await bcrypt.hash(password,salt)
-const UserData = new User({
-    name:name,
-    email:email,
-    password :hashPass,
-    tc:tc
-})
-const UserDataSave = await UserData.save()
-const findsavedUser =  await User.findOne({email})
-// Generate JWT token 
-const token = jwt.sign({userID:findsavedUser._id}, process.env.JWT_SECRET_KEY,{expiresIn:'5d' })
-res.send({"token":token})
-}else{
-    res.status(401).send('Password does not match')
-
-}
-        }else{
-            res.status(401).send('All fields are required')
+    static UserRegisteration = async (req, res) => {
+        // handle request body  parameters error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
-    }
-}
+        const { name, email, password, password_confirm, tc } = req.body
+        const emailfind = await User.findOne({ email })
+        if (emailfind) {
+            res.send("Email already exists, please enter another email")
+        } else {
+            if (name && email && password && password_confirm && tc) {
 
-//  User login 
-static UserLogin = async (req,res)=>{
-    try {
-        const {email, password} = req.body
-
-        if(email && password){
-            const user = await User.findOne({email})
-            if(user !=null){
-                const isMatch = await bcrypt.compare(password,user.password)
-                if(user.email === email && isMatch){
-                    //  token gnerate for login
-                    const token = jwt.sign({userID:user._id}, process.env.JWT_SECRET_KEY,{expiresIn:'5d' })
-res.status(200).send({"token":token})
-                    
-                }else{
-                    res.status(200).send("Invalid credentials")
+                if (password === password_confirm) {
+                    const salt = await bcrypt.genSalt(10)
+                    const hashPass = await bcrypt.hash(password, salt)
+                    const UserData = new User({
+                        name: name,
+                        email: email,
+                        password: hashPass,
+                        tc: tc
+                    })
+                    const UserDataSave = await UserData.save()
+                    const findsavedUser = await User.findOne({ email })
+                    // Generate JWT token 
+                    const token = jwt.sign({ userID: findsavedUser._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+                    res.send({ "token": token })
+                } else {
+                    res.status(401).send('Password does not match')
 
                 }
-
-            }else{
-                res.status(400).send("User does'not exists")
+            } else {
+                res.status(401).send('All fields are required')
             }
-        }else{
-            res.status(400).send("All field are required")
         }
-    } catch (error) {
-        
     }
-}
-// user change password 
-static changePassword = async(req,res)=>{
-    const {password, password_confirm}= req.body
-    if(password && password_confirm){
-if(password === password_confirm){
-    const salt =await  bcrypt.genSalt(10)
-    const hashPass = await bcrypt.hash(password,salt)
-    console.log(req.user)
-    await User.findByIdAndUpdate(req.user._id, {$set:{password:hashPass}})
-    res.send("Password changed successfully")
-}else{
 
-    res.status(400).send("password does not  match")
-}
-    }else{
-        res.status(400).send("All fields are required!")
+    //  User login 
+    static UserLogin = async (req, res) => {
+        try {
+            const { email, password } = req.body
+
+            if (email && password) {
+                const user = await User.findOne({ email })
+                if (user != null) {
+                    const isMatch = await bcrypt.compare(password, user.password)
+                    if (user.email === email && isMatch) {
+                        //  token gnerate for login
+                        const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
+                        res.status(200).send({ "token": token })
+
+                    } else {
+                        res.status(200).send("Invalid credentials")
+
+                    }
+
+                } else {
+                    res.status(400).send("User does'not exists")
+                }
+            } else {
+                res.status(400).send("All field are required")
+            }
+        } catch (error) {
+
+        }
     }
-}
-static LoggedUserData = (req,res)=>{
-    res.status(200).send(req.user)
-}
+    // user change password 
+    static changePassword = async (req, res) => {
+        const { password, password_confirm } = req.body
+        if (password && password_confirm) {
+            if (password === password_confirm) {
+                const salt = await bcrypt.genSalt(10)
+                const hashPass = await bcrypt.hash(password, salt)
+                console.log(req.user)
+                await User.findByIdAndUpdate(req.user._id, { $set: { password: hashPass } })
+                res.send("Password changed successfully")
+            } else {
+
+                res.status(400).send("password does not  match")
+            }
+        } else {
+            res.status(400).send("All fields are required!")
+        }
+    }
+    static LoggedUserData = (req, res) => {
+        res.status(200).send(req.user)
+    }
+
+    static sendEmail = async (req, res) => {
+        const { email } = req.body
+        if (email) {
+            const user = await User.findOne({ email })
+            if (user) {
+                const secret = user._id + process.env.JWT_SECRET_KEY
+                const token = jwt.sign({ userID: user._id }, secret, { expiresIn: '15m' })
+                const link = `http://127.0.0.1:3000/api/user/reset/${user._id}/${token}`
+                console.log(link)
+            } else {
+                res.send({ "status": "failed", "message": "Email doesn't exists " })
+
+            }
+        } else {
+            res.send({ "status": "failed", "message": "Email Field is Required" })
+        }
+    }
+
+    static passwordReset = async (req, res) => {
+        const { password, password_confirm } = req.body
+        console.log(req.body)
+        const { id, token } = req.params
+        console.log(req.params)
+        const user = await User.findById(id)
+        console.log(user)
+        const new_secret = user._id + process.env.JWT_SECRET_KEY
+        try {
+        jwt.verify(token, new_secret)
+            // let verifyToken = jwt.verify(token, new_secret)
+            // console.log(verifyToken)
+            if (password && password_confirm) {
+                if (password === password_confirm) {
+                    const salt = await bcrypt.genSalt(10)
+                    const newHashPassword = await bcrypt.hash(password, salt)
+                    await User.findByIdAndUpdate(user._id, { $set: { password: newHashPassword } })
+                    res.status(200).send({ "status": "success", "message": "Password Reset Successfully" })
+                } else {
+                    res.status(400).send("Password does not match!")
+                }
+            } else {
+                res.status(400).send("All field are required.")
+
+            }
+        } catch (error) {
+            res.status(400).send("Internal Server Error Occured")
+
+        }
+    }
 }
 
 export default UserController
--- routers/auth.js
+++ routers/auth.js
@@ -10,6 +10,8 @@ Authrouter.use('/LoggedUserData',Userauth)
 // Public routes
 Authrouter.post('/register', [body("name", "Enter a valid name.").isLength({ min: 3 })], UserController.UserRegisteration)
 Authrouter.post('/login', UserController.UserLogin)
+Authrouter.post('/sendEmail', UserController.sendEmail)
+Authrouter.post('/passwordReset/:id/:token', UserController.passwordReset)
 // protected  routes
 Authrouter.post('/changePassword', UserController.changePassword)
 Authrouter.post('/LoggedUserData', UserController.LoggedUserData)
